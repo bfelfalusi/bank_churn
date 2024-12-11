13293d794fef62a29ae6c8ff18c974a52b7db92e
b.felfalusi@stud.uni-obuda.hu <b.felfalusi@stud.uni-obuda.hu>
2024-12-12 00:37:01 +0100

    basics

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 import streamlit as st
 import pickle
 
+
+st.title("Bank Customer Churn Prediction")
 gender = st.radio("What is the gender?",("Male","Female"))
+if gender == "Male":
+    gender = 1
+else:
+    gender = 0
+button2 = st.button("press")
+if button2:
+    st.write(gender)
+predictbutton = st.button("Predict")
