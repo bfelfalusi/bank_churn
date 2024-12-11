0e149888a08542eba9348ece8e8d3fae4d38d09d
b.felfalusi@stud.uni-obuda.hu <b.felfalusi@stud.uni-obuda.hu>
2024-12-11 17:07:10 +0100

    s

--- app.py
+++ app.py
@@ -2,5 +2,7 @@ import streamlit as st
 st.title("streamlit app tut")
 st.write("this is my new app")
 button1 = st.button("Predict")
+if button1:
+    st.write("button pressed")
 st.header("(estimated) Income")
 est_inc =  st.slider("what is the estimated income?",0,4)
