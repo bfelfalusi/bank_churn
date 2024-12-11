bbcabf817bf92c0865c60388acea9b8d7c709df6
b.felfalusi@stud.uni-obuda.hu <b.felfalusi@stud.uni-obuda.hu>
2024-12-12 00:41:08 +0100

    basics2

--- app.py
+++ app.py
@@ -4,5 +4,10 @@ import pickle
 
 st.title("Bank Customer Churn Prediction")
 gender = st.radio("What is the gender?",("Male","Female"))
-
+if gender == 'Male':
+    genderb = 1
+else:
+    genderb = 0
 predictbutton = st.button("Predict")
+if predictbutton:
+    st.write(genderb)
