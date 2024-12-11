2c3f80c67cdf4577038c4622fa8a37cd68f22a2d
b.felfalusi@stud.uni-obuda.hu <b.felfalusi@stud.uni-obuda.hu>
2024-12-11 17:05:42 +0100

    slider1

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
 st.title("streamlit app tut")
 st.write("this is my new app")
-button1 = st.button("Predictđ")
+button1 = st.button("Predict")
+st.header("(estimated) Income")
+est_inc =  st.slider("what is the estimated income?",0,4)
