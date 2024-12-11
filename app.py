bbc3517bf15492614307cca9ca73c55c652a0874
b.felfalusi@stud.uni-obuda.hu <b.felfalusi@stud.uni-obuda.hu>
2024-12-11 21:41:23 +0100

    ab

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
 import streamlit as st
 import pickle
 
-@st.cache(allow_output_mutation=True)
-def load_model(model_name):
-    with open(model_name, 'rb') as file:
-        loaded_model = pickle.load(file)
-    return loaded_model
-
-model = load_model('best_rf_model.pkl')
-
 gender = st.radio("What is the gender?",("Male","Female"))
