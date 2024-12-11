1b02a7f5d6463c71d08d0528d854d4f987a679bb
b.felfalusi@stud.uni-obuda.hu <b.felfalusi@stud.uni-obuda.hu>
2024-12-12 00:37:44 +0100

    git commit -m 'yes'

--- app.py
+++ app.py
@@ -4,11 +4,5 @@ import pickle
 
 st.title("Bank Customer Churn Prediction")
 gender = st.radio("What is the gender?",("Male","Female"))
-if gender == "Male":
-    gender = 1
-else:
-    gender = 0
-button2 = st.button("press")
-if button2:
-    st.write(gender)
+
 predictbutton = st.button("Predict")
