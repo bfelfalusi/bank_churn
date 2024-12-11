3bcf0b40fb8046dfe74a48bf21617bcbc21d5d09
b.felfalusi@stud.uni-obuda.hu <b.felfalusi@stud.uni-obuda.hu>
2024-12-12 00:44:39 +0100

    slide

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ import streamlit as st
 import pickle
 
 
-st.title("Bank Customer Churn Prediction")
+st.markdown("<h1 style='text-align: center; color: white;'>Bank Customer Churn Prediction</h1>", unsafe_allow_html=True)
+credit_score = st.slider("credit_score",0,850)
 gender = st.radio("What is the gender?",("Male","Female"))
 if gender == 'Male':
     genderb = 1
 else:
     genderb = 0
+
+
 predictbutton = st.button("Predict")
-if predictbutton:
-    st.write(genderb)
