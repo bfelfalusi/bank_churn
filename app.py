59d9d6c01a62401e2e4b2fc83b499f0c22bb0d72
b.felfalusi@stud.uni-obuda.hu <b.felfalusi@stud.uni-obuda.hu>
2024-12-12 00:47:34 +0100

    eag2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pickle
 st.markdown("<h1 style='text-align: center; color: white;'>Bank Customer Churn Prediction</h1>", unsafe_allow_html=True)
 credit_score = st.slider("Credit Score",350,850)
 gender = st.radio("Gender",("Male","Female"))
-age = st.number_input("Age")
+age = st.number_input("Age",0,100,int)
 if gender == 'Male':
     genderb = 1
 else:
