20a69f6feec3df549d7e13ec2c7bcaccf6d2d0f6
b.felfalusi@stud.uni-obuda.hu <b.felfalusi@stud.uni-obuda.hu>
2024-12-12 01:10:00 +0100

    writing

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ credit_score = st.slider("Credit Score",350,850)
 gender = st.radio("Gender",("Male","Female"))
 age = st.number_input("Age",18,100)
 tenure = st.number_input("Tenure",0,10)
+st.write("0-50000: 0, 50001-100000: 1 ... 200001-250000: 4")
 balance = st.number_input("Balance",0,4)
-salary = st.number_input("Estimated Salary",0,4)
 credit_card = st.radio("Do you have a credit card?",("Yes","No"))
 active_member = st.radio("Are you currently customer of the bank?",("Yes","No"))
+st.write("0-40000: 0, 40001-80000: 1 ... 160001-200000: 4")
+salary = st.number_input("Estimated Salary",0,4)
 if gender == 'Male':
     genderb = 1
 else:
