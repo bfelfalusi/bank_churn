1f21c699dcaa2d276a1738554c7ded4ff3497fe4
b.felfalusi@stud.uni-obuda.hu <b.felfalusi@stud.uni-obuda.hu>
2024-12-12 01:03:06 +0100

    chab

--- app.py
+++ app.py
@@ -9,10 +9,19 @@ age = st.number_input("Age",18,100)
 tenure = st.number_input("Tenure",0,10)
 balance = st.number_input("Balance",0,4)
 salary = st.number_input("Estimated Salary",0,4)
+credit_card = st.radio("Do you have a credit card?",("Yes","No"))
+active_member = st.radio("Are you currently customer of the bank?",("Yes","No"))
 if gender == 'Male':
     genderb = 1
 else:
     genderb = 0
-
+if active_member == "Yes":
+    activeb = 1
+else:
+    activeb = 0
+if credit_card == "Yes":
+    creditb = 1
+else:
+    creditb = 0
 
 predictbutton = st.button("Predict")
