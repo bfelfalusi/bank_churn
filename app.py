0ec26936ea490abe7399f4582b8994b68e172064
b.felfalusi@stud.uni-obuda.hu <b.felfalusi@stud.uni-obuda.hu>
2024-12-12 01:57:25 +0100

    idk

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import requests
 
 model_url = "https://github.com/bfelfalusi/bank_churn/blob/main/best_vclf_model.pkl"
 response = requests.get(model_url)
-with open('model.pkl','wb') as f:
+with open('best_vclf_model.pkl','wb') as f:
     f.write(response.content)
 
-with open('model.pkl','rb') as f:
+with open('best_vclf_model.pkl','rb') as f:
     model = pickle.load(f)
 
 st.markdown("<h1 style='text-align: center; color: white;'>Bank Customer Churn Prediction</h1>", unsafe_allow_html=True)
