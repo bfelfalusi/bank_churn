b6b3dcb7d9be4dc223e0a9bc7e5ef1b82b0e6f9f
b.felfalusi@stud.uni-obuda.hu <b.felfalusi@stud.uni-obuda.hu>
2024-12-16 13:23:41 +0100

    almost_last

--- app.py
+++ app.py
@@ -1,67 +1,66 @@
 import streamlit as st
 import pickle
+import numpy as np
 import requests
 import urllib.request
 
+loaded_model = pickle.load(open('best_vclf_model.pkl','rb'))
 
-url = "https://github.com/bfelfalusi/bank_churn/blob/main/best_vclf_model.pkl"
+def main():
+    st.markdown("<h1 style='text-align: center; color: white;'>Bank Customer Churn Prediction</h1>", unsafe_allow_html=True)
+    credit_score = st.slider("Credit Score",350,850)
+    gender = st.radio("Gender",("Male","Female"))
+    age = st.number_input("Age",18,100)
+    tenure = st.number_input("Tenure",0,10)
+    balance = st.number_input("Balance:  0-50000: 0, 50001-100000: 1 ... 200001-250000: 4",0,4)
+    product_num = st.number_input("Number of products owned",1,4)
+    credit_card = st.radio("Do you have a credit card?",("Yes","No"))
+    active_member = st.radio("Are you currently customer of the bank?",("Yes","No"))
+    salary = st.number_input("Estimated Salary:  0-40000: 0, 40001-80000: 1 ... 160001-200000: 4",0,4)
+    country = st.selectbox("Country",("Germany","Spain","France"))
+    if gender == 'Male':
+        genderb = 1
+    else:
+        genderb = 0
+    if active_member == "Yes":
+        activeb = 1
+    else:
+        activeb = 0
+    if credit_card == "Yes":
+        creditb = 1
+    else:
+        creditb = 0
+    if country.lower() == 'germany':
+        countryb = (1,0)
+    elif country.lower() == 'spain':
+        countryb= (0,1)
+    else:
+        countryb = (0,0)
 
-with open('best_vclf_model.pkl', 'rb') as file:
-    loaded_model = pickle.load(file)
+    features = [[]]
+    predictbutton = st.button("Predict")
 
+    st.write("Bank churn result: ")
 
-st.markdown("<h1 style='text-align: center; color: white;'>Bank Customer Churn Prediction</h1>", unsafe_allow_html=True)
-credit_score = st.slider("Credit Score",350,850)
-gender = st.radio("Gender",("Male","Female"))
-age = st.number_input("Age",18,100)
-tenure = st.number_input("Tenure",0,10)
-balance = st.number_input("Balance:  0-50000: 0, 50001-100000: 1 ... 200001-250000: 4",0,4)
-product_num = st.number_input("Number of products owned",1,4)
-credit_card = st.radio("Do you have a credit card?",("Yes","No"))
-active_member = st.radio("Are you currently customer of the bank?",("Yes","No"))
-salary = st.number_input("Estimated Salary:  0-40000: 0, 40001-80000: 1 ... 160001-200000: 4",0,4)
-country = st.selectbox("Country",("Germany","Spain","France"))
-if gender == 'Male':
-    genderb = 1
-else:
-    genderb = 0
-if active_member == "Yes":
-    activeb = 1
-else:
-    activeb = 0
-if credit_card == "Yes":
-    creditb = 1
-else:
-    creditb = 0
-if country.lower() == 'germany':
-    countryb = (1,0)
-elif country.lower() == 'spain':
-    countryb= (0,1)
-else:
-    countryb = (0,0)
+    if predictbutton:
+        features[0].append(credit_score)
+        features[0].append(genderb)
+        features[0].append(age)
+        features[0].append(tenure)
+        features[0].append(balance)
+        features[0].append(product_num)
+        features[0].append(creditb)
+        features[0].append(activeb)
+        features[0].append(salary)
+        features[0].append(countryb[0])
+        features[0].append(countryb[1])
+        
 
-lista = [[]]
-predictbutton = st.button("Predict")
+    prediction = loaded_model.predict(features)
+    if prediction == 0:
+        st.write("Ez az ügyfél maradt a banknál.")
+    else:
+        st.write("Ez az ügyfél otthagyta a bankot.") 
 
-st.write("Bank churn result: ")
-
-if predictbutton:
-    lista[0].append(credit_score)
-    lista[0].append(genderb)
-    lista[0].append(age)
-    lista[0].append(tenure)
-    lista[0].append(balance)
-    lista[0].append(product_num)
-    lista[0].append(creditb)
-    lista[0].append(activeb)
-    lista[0].append(salary)
-    lista[0].append(countryb[0])
-    lista[0].append(countryb[1])
-    # random_num = random.randint(0,1)
-    # if random_num == 0:
-    #     st.write("Az ügyfél maradt a banknál.")
-    # else:
-    #     st.write("Az ügyfél otthagyta a bankot.")
-    
-
-y_pred = loaded_model.predict(lista)
+if __name__ == '__main__':
+        main()
