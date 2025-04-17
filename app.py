e9d19f1f64960395b707217827e68aa27fa04ceb
bfelfalusi <148013634+bfelfalusi@users.noreply.github.com>
2025-04-17 12:54:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def main():
         features[0].append(countryb[0])
         features[0].append(countryb[1])
 
-        y_scores_new = loaded_model.predict_proba(X_new)[:, 1]
+        y_scores_new = loaded_model.predict_proba(features)[:, 1]
 
         prediction = (y_scores_new >= 0.36).astype(int)
         
