b111a24debadc06ddbf603f7bc298e927df31249
bfelfalusi <148013634+bfelfalusi@users.noreply.github.com>
2025-04-17 09:49:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import numpy as np
-#import joblib
+import joblib
 import streamlit as st
 import pandas as pd
 
-#loaded_model = joblib.load('svc_joblib.joblib')
+loaded_model = joblib.load('svc_joblib.joblib')
 
 def main():
     st.markdown("<h1 style='text-align: center; color: white;'>Bank Customer Churn Prediction</h1>", unsafe_allow_html=True)
