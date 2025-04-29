662599d5a68a2de0a98e19b6a31d61f3fa08a648
bfelfalusi <148013634+bfelfalusi@users.noreply.github.com>
2025-04-29 14:18:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ def main():
     gender = st.radio("Gender",("Male","Female"))
     age = st.number_input("Age",18,100)
     tenure = st.number_input("Tenure",0,10)
-    balance = st.number_input("Balance:  0-50000€: 0, 50001€-100000€: 1 ... 200001€-250000€: 4",0,4)
+    balance = st.number_input("Balance:  0-50.000€: 0, 50.001€-100.000€: 1 ... 200.001€-250.000€: 4",0,4)
     product_num = st.number_input("Number of products owned",1,4)
     credit_card = st.radio("Do you have a credit card?",("Yes","No"))
     active_member = st.radio("Are you currently customer of the bank?",("Yes","No"))
-    salary = st.number_input("Estimated Salary:  0-40000€: 0, 40001€-80000€: 1 ... 160001€-200000€: 4",0,4)
+    salary = st.number_input("Estimated Salary (Yearly):  0-40.000€: 0, 40.001€-80.000€: 1 ... 160.001€-200.000€: 4",0,4)
     country = st.selectbox("Country",("Germany","Spain","France"))
     if gender == 'Male':
         genderb = 1
